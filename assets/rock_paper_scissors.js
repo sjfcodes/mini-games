eef7688d5006288ff03aac78ea7f61290db831d5
Samuel Fox <samueljasonfox@gmail.com>
2022-03-31 14:05:04 -0700

    rock paper scissors

---
+++ assets/rock_paper_scissors.js
@@ -0,0 +1,11 @@
+const playGame = (data) => {
+    let uChoice;
+    const cChoice = data.options[Math.floor(Math.random() * data.options.length)]
+    const handleResult = (key) => !(alert(`you chose ${uChoice} & computer chose ${cChoice}`)) && !(alert(key === 'tie' ? 'its a tie!' : `you ${key}!`)) && !(data[key]++) && data.rounds++
+    while (!data.options.includes(uChoice)) uChoice = prompt(`choose ${data.options}`).toLowerCase()
+    if (cChoice === uChoice) handleResult('tie')
+    else if (uChoice === 'r' && cChoice === 's' || uChoice === 'p' && cChoice === 'r' || uChoice === 's' && cChoice === 'p') handleResult('win')
+    else handleResult('lose')
+    return !(alert(`${JSON.stringify(data, null, 4)}`)) && confirm('play again?') && playGame(data)
+}
+document.querySelector('#r-p-s').addEventListener('click', () => !(alert('welcome to rock, paper, scissors!')) && playGame({ rounds: 0, win: 0, lose: 0, tie: 0, options: ['r', 'p', 's'] }));
