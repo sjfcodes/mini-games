36f083b5f6f25e53d9ed049dd487919b43070933
Samuel Fox <samueljasonfox@gmail.com>
2022-03-31 14:10:47 -0700

    update folder structure & add styling

--- assets/js/rock_paper_scissors.js
+++ assets/js/rock_paper_scissors.js
@@ -4,8 +4,8 @@ const playGame = (data) => {
     const handleResult = (key) => !(alert(`you chose ${uChoice} & computer chose ${cChoice}`)) && !(alert(key === 'tie' ? 'its a tie!' : `you ${key}!`)) && !(data[key]++) && data.rounds++
     while (!data.options.includes(uChoice)) uChoice = prompt(`choose ${data.options}`).toLowerCase()
     if (cChoice === uChoice) handleResult('tie')
-    else if (uChoice === 'r' && cChoice === 's' || uChoice === 'p' && cChoice === 'r' || uChoice === 's' && cChoice === 'p') handleResult('win')
+    else if (uChoice === 'rock' && cChoice === 'scissors' || uChoice === 'paper' && cChoice === 'rock' || uChoice === 'scissors' && cChoice === 'paper') handleResult('win')
     else handleResult('lose')
     return !(alert(`${JSON.stringify(data, null, 4)}`)) && confirm('play again?') && playGame(data)
 }
-document.querySelector('#r-p-s').addEventListener('click', () => !(alert('welcome to rock, paper, scissors!')) && playGame({ rounds: 0, win: 0, lose: 0, tie: 0, options: ['r', 'p', 's'] }));
+document.querySelector('#r-p-s').addEventListener('click', () => !(alert('welcome to rock, paper, scissors!')) && playGame({ rounds: 0, win: 0, lose: 0, tie: 0, options: ['rock', 'paper', 'scissors'] }));
